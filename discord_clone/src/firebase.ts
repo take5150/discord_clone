b524afc25f517b1353c9a87c8abc8f46a5981942
take5150 <watanabetakehiro5150@gmail.com>
2024-01-06 06:56:43 +0900

    firebase setting

---
+++ discord_clone/src/firebase.ts
@@ -0,0 +1,27 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore/lite"
+import { GoogleAuthProvider, getAuth } from "firebase/auth"
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAnMUuDOmYbz-QS_RLc-h-V9PZDzurfCQE",
+  authDomain: "discord-clone-9c5c4.firebaseapp.com",
+  projectId: "discord-clone-9c5c4",
+  storageBucket: "discord-clone-9c5c4.appspot.com",
+  messagingSenderId: "588794185177",
+  appId: "1:588794185177:web:0ad9952a9fa5f3bb2145c8",
+  measurementId: "G-JXQ0V9NSLZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// DB情報取得
+const db = getFirestore(app);
+// 認証情報取得
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+export { auth, provider, db };
